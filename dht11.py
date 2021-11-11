8830292770030ac50c103a9069130f6690e741eb
TheIszer <theiszer@gmail.com>
2021-11-10 22:42:54 -0600

    make query function added

--- dht11.py
+++ dht11.py
@@ -2,9 +2,22 @@
 import time
 import board
 import adafruit_dht
+# Graphql
+import requests
 
 # Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT11(board.D17)
+
+# Function to make a post to graphql 
+def make_query(query, url, headers):
+    """
+    Make query response
+    """
+    request = requests.post(url, json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
 while True:
     try:
@@ -12,6 +25,7 @@ while True:
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
+        #r = requests.post('')
         print(
             "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
                 temperature_f, temperature_c, humidity
