3fe871160b89baea09471468954c1656e2293c2c
Ale Oliva <aleoliva9@hotmail.com>
2021-11-11 00:33:10 -0600

    initialize dht

--- ldr.py
+++ ldr.py
@@ -1,8 +1,12 @@
+# LDR sensor
 import RPi.GPIO as GPIO
 import time
+# Graphql
+import requests
+
+# Initial the dht device, with data pin connected to 23
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-
 LIGHT_PIN = 23
 GPIO.setup(LIGHT_PIN, GPIO.IN)
 
