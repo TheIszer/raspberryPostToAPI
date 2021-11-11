51866aca51bf6c1379e36cf4ee031af4896f0b99
Ale Oliva <aleoliva9@hotmail.com>
2021-11-11 00:22:59 -0600

    add source code #2

--- ldr.py
+++ ldr.py
@@ -2,3 +2,13 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+
+LIGHT_PIN = 23
+GPIO.setup(LIGHT_PIN, GPIO.IN)
+
+while True:
+    if GPIO.input(LIGHT_PIN):
+      print ('NO HAY LUZ')
+    else:
+      print ('LUZ') 
+    time.sleep(1)
