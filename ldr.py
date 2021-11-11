01c69a0631907ca725ad323884eab31680c0f258
Ale Oliva <aleoliva9@hotmail.com>
2021-11-11 00:34:20 -0600

    query function created

--- ldr.py
+++ ldr.py
@@ -10,6 +10,17 @@ GPIO.setwarnings(False)
 LIGHT_PIN = 23
 GPIO.setup(LIGHT_PIN, GPIO.IN)
 
+# Function to make a post to graphql 
+def make_query(query, url, headers):
+    """
+    Make query response
+    """
+    request = requests.post(url, json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
 while True:
     if GPIO.input(LIGHT_PIN):
       print ('NO HAY LUZ')
