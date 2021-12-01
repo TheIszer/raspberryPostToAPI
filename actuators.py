730a21ffad4852f6b4f918949821c24fcd937674
TheIszer <theiszer@gmail.com>
2021-12-01 17:30:16 -0600

    actuator.py updated

--- actuators.py
+++ actuators.py
@@ -4,3 +4,20 @@
     Description:    Code written in python that reads the value from an API
                     and use an actuator. Use graphql for the API. 
 '''
+# LED
+import RPi.GPIO as GPIO
+import time
+# Graphql
+import requests
+
+# Initial the LED device, with data pin connected to 24
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+LED_PIN = 24
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+while True:
+    GPIO.output(LED_PIN, True)
+    time.sleep(5.0)
+    GPIO.output(LED_PIN, False)
+    time.sleep(5.0)
