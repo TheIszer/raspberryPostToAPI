b6481b315846cae8e9973607a3642771f521b63c
TheIszer <theiszer@gmail.com>
2021-12-01 17:18:39 -0600

    "actuators branch created"

---
+++ actuators.py
@@ -0,0 +1,6 @@
+'''
+    Authors: Kaleb Antonio, Fernando Lopez, Alejandra Oliva, Asiel Trejo
+    Emails: A01732213, A07144620,  A01731592, A01731489   @tec.mx
+    Description:    Code written in python that reads the value from an API
+                    and use an actuator. Use graphql for the API. 
+'''
